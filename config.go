c2566dca46103378a4ea08830659bfcd6e61b7f2
李光春 <gc@dtapp.net>
2022-08-30 16:42:44 +0800

    - update log

---
+++ config.go
@@ -0,0 +1,8 @@
+package wechatminiprogram
+
+// ConfigApp 配置
+func (c *Client) ConfigApp(appId, appSecret string) *Client {
+	c.config.appId = appId
+	c.config.appSecret = appSecret
+	return c
+}
--- get.go
+++ get.go
@@ -2,19 +2,15 @@ package wechatminiprogram
 
 import "context"
 
-func (c *Client) getAppId() string {
-	return c.config.AppId
+func (c *Client) GetAppId() string {
+	return c.config.appId
 }
 
-func (c *Client) getAppSecret() string {
-	return c.config.AppSecret
+func (c *Client) GetAppSecret() string {
+	return c.config.appSecret
 }
 
 func (c *Client) getAccessToken(ctx context.Context) string {
-	c.config.AccessToken = c.GetAccessToken(ctx)
-	return c.config.AccessToken
-}
-
-func (c *Client) GetAppId() string {
-	return c.config.AppId
+	c.config.accessToken = c.GetAccessToken(ctx)
+	return c.config.accessToken
 }
